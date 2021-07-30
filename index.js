0b7e295d77b515c4c356e435dc3e6b884dd05d84
Wienert Gillijns <wienert.gillijns@student.ehb.be>
2021-07-30 21:22:57 +0200

    mongoose

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.dfwwe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
