5199fd98608e17afdff3191ac7aa0a9bf2760737
Wienert Gillijns <wienert.gillijns@student.ehb.be>
2021-07-30 22:26:15 +0200

    create categorie, logs, get all logs by categorie

--- index.js
+++ index.js
@@ -26,9 +26,41 @@ app.post('/categorie', (req, response) => {
         
     });
     });
+})
 
-  
+app.post('/logs', (req, response) => {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb+srv://admin:admin@cluster0.dfwwe.mongodb.net/Herkansing?retryWrites=true&w=majority";
 
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Herkansing");
+    var myobj = req.body
+    dbo.collection("Logs").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+        response.send(myobj)
+        
+    });
+    });
+})
+
+app.get('/logs', (req, response) => {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb+srv://admin:admin@cluster0.dfwwe.mongodb.net/Herkansing?retryWrites=true&w=majority";
+
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("Herkansing");
+    
+    var query = { categorieId: req.query.categorieId };
+    dbo.collection("Logs").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        response.send(result);
+        db.close();
+    });
+        });
 })
 
 app.listen(port, () => {
